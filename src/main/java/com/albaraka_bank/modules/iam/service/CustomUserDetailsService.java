25ae7b2746206d381d8fdd6a2310b265cee7c869
A-007481D <labidabdelmalek@gmail.com>
2025-12-16 16:00:00 +0100

    feat(security): Implement user details service loading from database #ABB-4 #done

---
+++ src/main/java/com/albaraka_bank/modules/iam/service/CustomUserDetailsService.java
@@ -0,0 +1,33 @@
+package com.albaraka_bank.modules.iam.service;
+
+import com.albaraka_bank.modules.iam.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        var user = userRepository.findByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+
+        return new org.springframework.security.core.userdetails.User(
+                user.getEmail(),
+                user.getPassword(),
+                user.isActive(),
+                true,
+                true,
+                true,
+                Collections.singletonList(new SimpleGrantedAuthority("ROLE_" + user.getRole().name())));
+    }
+}
