b74855bbb6e490d0c2d373727e8cfc9224dec09a
A-007481D <labidabdelmalek@gmail.com>
2025-12-22 14:00:00 +0100

    feat(account): Implement Client Account View #ABB-28 #done

--- src/main/java/com/albaraka_bank/modules/account/controller/AccountController.java
+++ src/main/java/com/albaraka_bank/modules/account/controller/AccountController.java
@@ -1,4 +1,34 @@
 package com.albaraka_bank.modules.account.controller;
 
+import com.albaraka_bank.common.util.SecurityUtils;
+import com.albaraka_bank.modules.account.dto.AccountResponse;
+import com.albaraka_bank.modules.account.model.Account;
+import com.albaraka_bank.modules.account.service.AccountService;
+import com.albaraka_bank.modules.iam.model.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/client/me")
+@RequiredArgsConstructor
 public class AccountController {
+
+    private final AccountService accountService;
+
+    @GetMapping("/account")
+    public ResponseEntity<AccountResponse> getMyAccount() {
+        User user = SecurityUtils.getCurrentUser();
+        Account account = accountService.getAccountByOwner(user);
+
+        AccountResponse response = AccountResponse.builder()
+                .accountNumber(account.getAccountNumber())
+                .balance(account.getBalance())
+                .ownerName(user.getFullName())
+                .build();
+
+        return ResponseEntity.ok(response);
+    }
 }
